206cd6612ec609ad11b6b72dcd043c0ad6944f53
Kristian Gunder Kramås <kristiankramas@outlook.com>
2025-03-17 15:45:25 +0100

    Add OpenAPI integration and React Query setup with persistence

--- README.md
+++ README.md
@@ -2,10 +2,45 @@
 
 Personal starter for experiments
 
+Install [bun](https://bun.sh/)
+
+Then run:
+
 ```
 bun i
 ```
 
 ```
 bun dev
+```
+
+## Optional sync with OpenAPI (Swagger)
+
+Included is a way to generate a [TanStack query](https://tanstack.com/query/latest) client from your schema for type-safe integration with your backend, using [OpenAPI TypeScript](https://openapi-ts.dev/)
+
+Adjust synchronization script and `fetchClient` to point to your OpenAPI spec before running the sync:
+
+```json
+// package.json
+
+{
+  "scripts": {
+    // Update this
+    "sync": "bun run openapi-typescript http://localhost:8888/openapi.json -o ./src/api/schema.d.ts"
+  }
+}
+```
+
+```tsx
+// src/api/index.tsx
+
+const fetchClient = createFetchClient<paths>({
+  // Update this
+  baseUrl: "http://localhost:8888/",
+});
+```
+
+
+```
+bun sync
 ```
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import "@/styles/reset.css";
 import "@/styles/tailwind.css";
 import "@/styles/styles.css";
 import App from "./App";
+import { QueryClient } from "@tanstack/react-query";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 if (import.meta.env.DEV) {
 	import("react-scan").then(({ scan }) => {
@@ -11,12 +15,30 @@ if (import.meta.env.DEV) {
 	});
 }
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			gcTime: 1000 * 60 * 60 * 24, // 24 hours
+		},
+	},
+});
+
+const persister = createSyncStoragePersister({
+	storage: window.localStorage,
+});
+
 const root = document.getElementById("root");
 
 if (root) {
 	createRoot(root).render(
 		<StrictMode>
-			<App />
+			<PersistQueryClientProvider
+				client={queryClient}
+				persistOptions={{ persister }}
+			>
+				<App />
+				<ReactQueryDevtools />
+			</PersistQueryClientProvider>
 		</StrictMode>,
 	);
 }
