494e050c17e05b68a840fc2bd142c0fb21e2f544
Kristian Gunder Kramås <kristiankramas@outlook.com>
2025-03-17 15:55:29 +0100

    Motion example

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,14 +1,26 @@
 import type { ComponentProps } from "react";
 import { cn } from "@/styles/utils";
+import { motion } from "motion/react";
 
 export type HelloWorldProps = {
 	//
-} & Omit<ComponentProps<"div">, "children">;
+} & Omit<ComponentProps<typeof motion.div>, "children">;
 
 export function HelloWorld({ className, ...props }: HelloWorldProps) {
 	return (
-		<div className={cn("", className)} {...props}>
+		<motion.div
+			className={cn("", className)}
+			initial={{
+				scale: 0,
+				opacity: 0,
+			}}
+			animate={{
+				scale: 1,
+				opacity: 1,
+			}}
+			{...props}
+		>
 			Hello, World!
-		</div>
+		</motion.div>
 	);
 }
