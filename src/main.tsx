55fd0f889e3f31a483fb1b2bb9a305a7d2ace4f2
Kristian Gunder Kramås <kristiankramas@outlook.com>
2025-03-17 14:30:32 +0100

    Add react-scan

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,12 @@ import "@/styles/tailwind.css";
 import "@/styles/styles.css";
 import App from "./App";
 
+if (import.meta.env.DEV) {
+	import("react-scan").then(({ scan }) => {
+		scan();
+	});
+}
+
 const root = document.getElementById("root");
 
 if (root) {
