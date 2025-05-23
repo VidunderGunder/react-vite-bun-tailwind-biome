a85fd37174562e4a37f997f92c7712be6723f110
Kristian Gunder Kramås <kristiankramas@outlook.com>
2025-05-23 15:45:42 +0200

    Remove comment

--- src/types/reset.d.ts
+++ src/types/reset.d.ts
@@ -3,12 +3,6 @@ import "@total-typescript/ts-reset/dom";
 
 declare global {
 	interface ObjectConstructor {
-		/**
-		 * Returns an array of a given object's own enumerable property names,
-		 * iterated in the same order that a normal loop would.
-		 *
-		 * This augmented signature attempts to provide more specific key types.
-		 */
 		keys<T extends Record<string, unknown>>(o: T): (keyof T | (string & {}))[];
 	}
 }
